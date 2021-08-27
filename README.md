c1a9c94fee2c9bf12877b676fce07e37f0f11f67
Bryce Dorn <brycedorn@gmail.com>
2021-08-27 16:06:51 -0500

    initial commit 📈

---
+++ README.md
@@ -0,0 +1,6 @@
+[market-vibe](https://bryce.io/market-vibe)
+
+# Running locally
+
+	  npm i
+		npm start
---
+++ index.js
@@ -0,0 +1,83 @@
+import { render, h } from 'preact';
+import htm from 'htm';
+import { useState, useEffect } from 'preact/hooks';
+
+const token = "c4kkb2aad3ib58s0a91g";
+const emoji = ["😖","☹️","🙁","😐","🙂","😄","🤤"];
+
+function getEmoji(change) {
+  if (change < -5) {
+    return emoji[0];
+  } else if (change < -3) {
+    return emoji[1];
+  } else if (change < -1) {
+    return emoji[2];
+  } else if (change > -1 && change < 1) {
+    return emoji[3];
+  } else if (change > 1) {
+    return emoji[4];
+  } else if (change > 3) {
+    return emoji[5];
+  } else if (change > 5) {
+    return emoji[5];
+  }
+}
+
+function getColor(change, opacityOverride) {
+  const positive = change >= 0;
+  const opacity = Math.abs(change * 20) / 100;
+  return `rgba(${positive ? 0 : 255}, ${positive ? 255 : 0}, 0, ${opacityOverride || opacity})`;
+}
+
+function getContainerStyle(change) {
+  return {
+    width: '100%',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: getColor(change),
+    fontFamily: 'sans-serif'
+  }
+};
+
+function getEmojiStyle() {
+  return {
+    fontSize: '12em',
+    margin: 0
+  }
+}
+
+function getTextStyle(change) {
+  return { 
+    borderRadius: '40px',
+    background: 'white',
+    padding: '30px 20px 20px 20px',
+    color: getColor(change),
+    fontSize: '4em'
+  }
+}
+
+function App (props) {
+  const [change, setChange] = useState(0);
+  
+  useEffect(() => {
+    fetch(`https://finnhub.io/api/v1/quote?symbol=SPY&token=${token}`)
+      .then(res => res.json())
+      .then(data => setChange(data.dp.toFixed(2)));
+  }, []);
+
+  useEffect(() => {
+    document.title = `Market is ${getEmoji(change)} today.`
+  }, [change])
+  
+  return (
+    <div id="test" style={getContainerStyle(change)}>
+      <h1 style={getEmojiStyle()}>{getEmoji(change)}</h1>
+      <h2 style={getTextStyle(change)}>{change >= 0 ? "+" : ""}{change}%</h2>
+    </div>
+  );
+}
+
+render(htm.bind(h)`<${App} />`, document.body);
